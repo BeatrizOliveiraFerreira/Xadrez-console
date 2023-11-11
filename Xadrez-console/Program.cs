3e3f7705112d9af65c0b86ee3011161d527d53b6
Beatriz <beaferreira.trabalho@gmail.com>
2023-11-10 21:44:24 -0300

    Add project files.

---
+++ Xadrez-console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using xadrez;
+using tabuleiro;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+
+namespace Xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                PartidaXadrez partida = new PartidaXadrez();
+                while (!partida.terminada) {
+                    try {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizarJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
